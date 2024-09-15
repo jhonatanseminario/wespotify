24ae3cf3299b6c207bd0149a6ce08811b7f10139
Jhonatan Seminario <seminariodev@gmail.com>
2024-09-15 02:08:44 -0500

    implementar función para obtener token de acceso a la api de spotify

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('¡Hola, mundo!');
+async function getToken() {
+    const clientId = '';
+    const clientSecret = '';
+    const endpoint = 'https://accounts.spotify.com/api/token';
+    const credentials = btoa(`${clientId}:${clientSecret}`);
+  
+    try {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${credentials}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                grant_type: 'client_credentials'
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const token = data.access_token;
+        return token;
+    } catch (error) {
+        console.error('Error fetching token:', error);
+    }
+}
+  
+getToken();
