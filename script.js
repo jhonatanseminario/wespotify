453997b5515b57f9fc512be6058c0916c2c18355
Jhonatan Seminario <seminariodev@gmail.com>
2024-09-15 02:53:41 -0500

    implementar función para buscar artistas

--- script.js
+++ script.js
@@ -27,5 +27,25 @@ async function getToken() {
         console.error('Error fetching token:', error);
     }
 }
-  
-getToken();
+
+async function searchArtists(artist) {
+    const token = await getToken();
+
+    try {
+        const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artist)}&type=artist`, {
+            headers: { 'Authorization': `Bearer ${token}` }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const artistsArray = data.artists.items.map(item => item.name);
+        console.log(artistsArray);
+    } catch(error) {
+        console.error('Error fetching artists:', error);
+    }
+}
+
+searchArtists('Kanye West')
